b7826fd02b81ac3a8abd55a0a20e78fc7b9cf284
Kian <kianbadiei@gmail.com>
2021-12-10 02:48:52 -0500

    Background music, Button Music, FootstepMusic.

--- Assets/Character/Scripts/PlayerBehaviour.cs
+++ Assets/Character/Scripts/PlayerBehaviour.cs
@@ -14,13 +14,21 @@ public class PlayerBehaviour : MonoBehaviour
 
     private Rigidbody2D rigidbody;
     Animator animator;
+    bool isMoving = false;
 
+    [SerializeField]
+    private AudioClip footStep;
+   
+
+    [SerializeField]
+    private AudioSource musicSrc;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        musicSrc = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -35,13 +43,26 @@ public class PlayerBehaviour : MonoBehaviour
         
         if(Mathf.Abs(rigidbody.velocity.x) < 0.01 && Mathf.Abs(rigidbody.velocity.y) < 0.01)
         {
-            animator.SetBool("IsMoving", false);
+           
+            isMoving = false;
         }
         else{
-            animator.SetFloat("Vely", Input.GetAxisRaw("Vertical"));
-            animator.SetFloat("Velx", Input.GetAxisRaw("Horizontal"));
-            animator.SetBool("IsMoving", true);
+          
+            isMoving = true;
         }
+        animator.SetFloat("Vely", Input.GetAxisRaw("Vertical"));
+        animator.SetFloat("Velx", Input.GetAxisRaw("Horizontal"));
+        animator.SetBool("IsMoving", isMoving);
+        if (isMoving)
+        {
+            if (!musicSrc.isPlaying)
+            {
+                musicSrc.Play();
+            }
+        }
+        else
+            musicSrc.Stop();
+
     }
 
 
